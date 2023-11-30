d0ebb688abdfd422e2dbfb11f5c606eb0af7a1df
Horizon1026 <lijiapeng_atlantis@163.com>
2023-12-01 00:00:14 +0800

    update code

--- src/visualizor.cpp
+++ src/visualizor.cpp
@@ -19,4 +19,35 @@ Visualizor::~Visualizor() {
     glfwTerminate();
 }
 
+void Visualizor::ErrorCallback(int32_t error, const char *description) {
+    ReportError("[Visualizor] Error detected :" << description);
+}
+
+void Visualizor::KeyboardCallback(GLFWwindow *window, int32_t key, int32_t scan_code, int32_t action, int32_t mods) {
+    if (action == GLFW_PRESS && key == GLFW_KEY_ESCAPE) {
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+    } else if (action == GLFW_PRESS) {
+        Visualizor::some_key_pressed_ = true;
+    }
+}
+
+VisualizorWindow *Visualizor::GetWindowPointer(const std::string &title, int32_t width, int32_t height) {
+    auto item = Visualizor::windows_.find(title);
+    if (item == Visualizor::windows_.end()) {
+        // If window with selected title is not found, create a new window.
+        auto iter = Visualizor::windows_.insert(std::make_pair(title, VisualizorWindow()));
+        iter.first->second.glfw_window = glfwCreateWindow(width, height, title.c_str(), nullptr, nullptr);
+
+        // If insert failed, clear it.
+        if (iter.first->second.glfw_window == nullptr) {
+            Visualizor::windows_.erase(title);
+            return nullptr;
+        }
+        return &(iter.first->second);
+    } else {
+        // Return the exist window.
+        return &(item->second);
+    }
+}
+
 }
